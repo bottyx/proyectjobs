ab0c719782ab499bae38912a794c8836e572d07d
Paco <francisco0611@hotmail.com>
2015-03-29 18:24:06 -0600

    add publish view interface

--- necesitoempleo/application/views/publish_view.php
+++ necesitoempleo/application/views/publish_view.php
@@ -1,9 +1,74 @@
 <div class="container">
-	<h1>Publicar trabajo ( Paco )</h1>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<h3>Nuevo empleo disponible</h3>
+			<form role="form" method="POST" action="#">
+				<div class="row">
+					<div class="col-md-6 col-lg-6">
+						<div class="form-group">
+							<label>Título</label>
+							<input id="txtAnio" name="txtAnio" type="text" maxlength="4" value="" class="form-control input-lg">
+						</div>
+					</div>
+
+					<div class="col-md-6 col-lg-6">
+						<div class="form-group">
+							<label>Categoría</label>
+							<select id="cboCategoria" name="cboCategoria" class="form-control input-lg">
+								<option value="0">Seleccionar categoria</option>
+							</select>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 col-lg-6">
+						<div class="form-group">
+							<label>Estado</label>
+							<select id="cboEstado" name="cboEstado" class="form-control input-lg">
+								<option value="0">Seleccionar estado</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="col-md-6 col-lg-6">
+						<div class="form-group">
+							<label>Ciudad</label>
+							<select id="cboMunicipio" name="cboMunicipio" class="form-control input-lg">
+								<option value="0">Seleccionar Ciudad</option>
+							</select>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-lg-12">
+						<div class="form-group">
+							<label>Descripcion</label>
+							<textarea class="form-control" rows="3"></textarea>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-lg-12">
+						<div class="form-group">
+							<label>Oferta</label>
+							<textarea class="form-control" rows="3"></textarea>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-lg-12">
+						<div class="form-group">
+							<label>Requisitos</label>
+							<textarea class="form-control" rows="3"></textarea>
+						</div>
+					</div>
+				</div>
+
+			</form>
+		</div>
+	</div>
 </div>
