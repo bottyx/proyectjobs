ec9442da7b5483b35cc1c94d4b9144ef5f67fe35
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2015-04-18 23:22:16 -0500

    LandingPage creada

--- necesitoempleo/application/views/home_view.php
+++ necesitoempleo/application/views/home_view.php
@@ -1,9 +1,153 @@
-<div class="container">
-	<h1>Vista de Home ( Tucan )</h1>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=no" />
+<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/css_dhVX4o9CWOmk6JLGhNYvcNTE_HU1FnEdFoq384Fz12Q.css" media="all" />
+<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/css_rmZUUWjwASvWtZodEQ8pIWoGK6gsHS23L3A9Qbs6wVc.css" media="all" />
+<!-- HTML5 element support for IE6-8 -->
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="<?php echo base_url();?>assets/js/js_0gj6QcpfRH2jzTbCQqf7kEkm4MXY0UA_sRhwPc8jC1o.js"></script>
+<script src="<?php echo base_url();?>assets/js/js_E77Ls0Xe1GIR2OLpq85xkclWmwyNal_hW6ksU08bimE.js"></script>
+<script>
+   (function($) 
+     {
+         $(document).ready(function() {
+             $("#md-slider-1").mdSlider();
+         });
+       })(jQuery);
+</script>
+<style type="text/css">
+.search{
+   padding-top: 20px;
+   height: 10%;
+   background: black;
+   z-index: 1;
+   position: relative;
+}
+</style>
+</head>
+<body class="html front not-logged-in one-sidebar sidebar-second page-node page-node- page-node-35 node-type-page navbar-is-static-top" >
+   <header id="navbar" role="banner" class="navbar navbar-static-top navbar-default">
+      <div class="container">
+         <div class="navbar-header">
+            <a class="logo navbar-btn pull-left" href="/jobboard/jobboard/" title="Home">
+            <img src="<?php echo base_url();?>assets/images/logo.png" alt="Home" />
+            </a>
+         </div>
+         <div class="post-job" > 
+            <a href="/jobboard/jobboard/user/register" class="btn btn-primary"><i class="fa fa-map-marker"></i>Publica un empleo</a>
+         </div>
+         <div class="navbar-collapse collapse">
+            <nav role="navigation">
+               <ul class="menu nav navbar-nav">
+                  <li class="first expanded active-trail dropdown active">
+                     <a href="/jobboard/jobboard/" title="JobBoard Home Page" class="active-trail dropdown-toggle active" data-target="#" data-toggle="dropdown">Principal</a>
+                  </li>
+                  <li class="leaf"><a href="/jobboard/jobboard/jobs" title="">Buscar empleos</a></li>
+                  <li class="leaf"><a href="/jobboard/jobboard/jobs" title="">En donde hay mas trabajo?</a></li>
+               </ul>
+            </nav>
+         </div>
+      </div>
+   </header>
+   <div class="highlighted">
+      <div class="region region-highlighted">
+         <section >
+            <div id="md-slider-1">
+               <div class="md-slide-item">
+                  <div class="md-mainimg"><img src="<?php echo base_url();?>assets/images/wallp2.jpg" alt="" /></div>
+               </div>
+            </div>
+            <div class="search">
+
+               <div class="container">
+                     <div class="col-lg-12">
+                        <div class="col-lg-3">
+                           <select class="form-control input-lg">...</select>
+                        </div> 
+
+                        <div class="col-lg-3">
+                           <select class="form-control input-lg">...</select>
+                        </div> 
+
+                        <div class="col-lg-6">
+
+                           <div class="col-lg-6">
+                              <input class="form-control input-lg" placeholder="Que trabajo buscas">
+                           </div> 
+
+                           <div class="col-lg-6">
+                              <button type="button" class="btn btn-primary btn-lg btn-block">Buscar</button>
+                           </div> 
+                        </div> 
+                     
+                  </div>
+               </div>
+
+            </div>
+         </section>
+         <!-- /.block -->
+      </div>
+   </div>
+   <div class="main-container container">
+   <header role="banner" id="page-header">
+   </header>
+   <!-- /#page-header -->
+   <div class="row">
+   <section class="col-sm-9">
+      <div class="region region-content">
+         <!-- /.block -->
+         <section id="block-views-job-listings-block-3" class="block block-views clearfix">
+            <div class="title-div">
+               <h2 class="block-title">Ultimos empleos agregados</h2>
+            </div>
+            <div class="view view-job-listings view-id-job_listings view-display-id-block_3 view-dom-id-edcf0799d059bdec4cf3a684612268d3">
+               <div class="view-content">
+
+
+                  <div class="views-row views-row-1 views-row-odd views-row-first row Featured">
+                     <div class="views-field views-field-edit-node">        <span class="field-content"></span>  </div>
+                     <div class="views-field views-field-nothing-1">
+                        <span class="field-content">
+                           <div class="col-md-12 col-sm-8 col-xs-12">
+                              <h3><a href="/jobboard/jobboard/job/interactive-media-developer">Mecanico especialista en Bochos</a></h3>
+                              <div class="features">
+                                 <span class="type"><i class="fa fa-briefcase"></i><span class="expire-label">Tipo:</span> <a href="/jobboard/jobboard/contract-type/full-time" typeof="skos:Concept" property="rdfs:label skos:prefLabel" datatype="">Tiempo completo</a></span><span class="expires"><i class="fa fa-clock-o"></i><span class="expire-label">Termina en:</span> 2 semanas</span>
+                              </div>
+                              <div class="description">
+                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                              </div>
+                           </div>
+                        </span>
+                     </div>
+                  </div>
+
+
+
+               </div>
+            </div>
+         </section>
+         <!-- /.block -->
+      </div>
+   </section>
+   <aside class="col-sm-3" role="complementary">
+   <div class="region region-sidebar-second">
+   <section id="block-views-sidebar-jobs-block-1" class="block block-views clearfix">
+      <div class="title-div">
+         <h2 class="block-title"># Empleos publicados</h2>
+      </div>
+   </section>
+   <!-- /.block -->
+   <section id="block-views-sidebar-jobs-block-3" class="block block-views clearfix">
+      <div class="title-div">
+         <h2 class="block-title"># Empleos disponibles</h2>
+      </div>
+   </section>
+   <!-- /.block -->
+</body>
+</html>
